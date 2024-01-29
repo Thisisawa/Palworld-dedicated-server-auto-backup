485a574e4c3156c2f640c7886097e0eea112320e
Thisisawa <87000861+Thisisawa@users.noreply.github.com>
2024-01-29 19:06:47 +0800

    Add files via upload

--- Lunch.bat
+++ Lunch.bat
@@ -2,6 +2,7 @@
 if "%1"=="hide" goto CmdBegin
 start mshta vbscript:createobject("wscript.shell").run("""%~0"" hide",0)(window.close)&&exit
 :CmdBegin
+
 chcp 65001
 
 REM 保存的備份數
@@ -10,26 +11,24 @@ REM 間隔(s)
 set interval="300"
 REM 備份檔資料夾 
 set main_folder="Pal\Saved\SaveGames"
-REM "Pal\Saved\SaveGames" === "Pal\Saved\SaveGames\"
+
 
 
 set proc="PalServer.exe"
 
 :start
-timeout /t 5
+timeout /t 0
+
 tasklist | findstr /i "%proc%"
-if errorlevel 1 (
-goto :end
-)
+if errorlevel 1 (goto :end)
 set YY=%DATE:~4%
-set "YY=%YY:/=%"
-set hr=%Time:~0,2%
+set YY=%YY:/=%
+if %Time:~0,2% leq 9 (set hr=0%Time:~1,1%) else (set hr=%Time:~0,2%)
 set min=%Time:~3,2%
 set sec=%Time:~6,2%
 set file_name="%YY%%MM%%DD%%hr%%min%%sec%.zip"
-set main_folder="Pal\Saved\SaveGames"
 set target_folder="Pal\Saved\SaveGames\0"
-set backup_files_folder="Pal\Saved\SaveGames\%file_name%"
+set backup_files_folder=%main_folder%\%file_name%
 PowerShell Compress-Archive -Path "%target_folder%" -DestinationPath "%backup_files_folder%"
 echo F|xcopy /Q /Y /F "%main_folder%%file_name%" "%main_folder%\2%file_name%"
 del "%main_folder%%file_name%"
